84f2357d5b8c2b0f16d9758101d2c84555de0475
ssst553 <stojanka.tanjevic@gmail.com>
2021-08-06 12:45:01 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/register', [RegisterController::class,'index'])->name('register');
+Route::post('/register', [RegisterController::class,'store']);
+
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
