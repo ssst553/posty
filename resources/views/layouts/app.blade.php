50b1841d398b1b846c577c4f43614cdc0fece873
ssst553 <stojanka.tanjevic@gmail.com>
2021-08-06 14:58:49 +0200

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,10 @@
             <a href="" class="p-3"> Korisnik </a>
         </li>
         <li>
-            <a href="" class="p-3"> Logout </a>
+        <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+             @csrf
+             <button type="submit">Logout</button>
+        </form>
         </li>
         @endauth
 
