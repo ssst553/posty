0ea0c6c22c17aa5a6a5a87ceb26d4f65343d26bd
ssst553 <stojanka.tanjevic@gmail.com>
2021-08-06 19:54:15 +0200

    kreiranje i prikaz postova

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,20 +12,20 @@
    <nav class="p-6 bg-white flex justify-between mb-6">
     <ul class="flex items-center">
         <li> 
-            <a href="" class="p-3"> Home </a>
+            <a href="/" class="p-3"> Home </a>
         </li>
         <li>
-            <a href="" class="p-3"> Dashboard </a>
+            <a href="{{route('dashboard')}}" class="p-3"> Dashboard </a>
         </li>
         <li>
-            <a href="" class="p-3"> Post </a>
+            <a href="{{route('posts')}}" class="p-3"> Posts </a>
         </li>
     </ul>
 
     <ul class="flex items-center">
        @auth
         <li>
-            <a href="" class="p-3"> Korisnik </a>
+            <a href="" class="p-3"> {{auth()->user()->name}} </a>
         </li>
         <li>
         <form action="{{ route('logout') }}" method="post" class="p-3 inline">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -10,7 +11,9 @@ Route::get('/', function(){
     return view('home');
 })->name('home');
 
-Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class,'index'])
+    ->name('dashboard');
+  //  ->middleware('auth');
 
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
@@ -20,7 +23,5 @@ Route::post('/login', [LoginController::class,'store']);
 Route::get('/register', [RegisterController::class,'index'])->name('register');
 Route::post('/register', [RegisterController::class,'store']);
 
-
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class,'index'])->name('posts');
+Route::post('/posts', [PostController::class,'store']);
